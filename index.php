7187b471daaa2d858aa93353f575c89bf8539a36
Error404 <khvysal@gmail.com>
2026-09-22 14:18:49 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Root Project Entrypoint Bridge (Allows http://localhost:8888/News-platefrom/ directly)
+ * news-platform / index.php
+ */
+
+require_once __DIR__ . '/public/index.php';
+?>
